c9e9f3af52135998dc17cd1c25d44b52faef7a1c
Rudra Thacker <thacker.rudra20@gmail.com>
2026-07-31 14:08:55 +0530

    Add floor and ceiling binary search solutions

--- find-ceiling-number/solution.java
+++ find-ceiling-number/solution.java
@@ -3,7 +3,7 @@ class solution {
         int s = 0;
         int e = arr.length-1;
         int ans = 0;
-        while(s<e){
+        while(s<=e){
             int mid = s+(e-s)/2;
             if(arr[mid]>key){
                 ans = arr[mid];
@@ -16,3 +16,18 @@ class solution {
         return ans; // also return s ; works in alternate of return ans.
     }
 }
+<<<<<<< HEAD
+=======
+/*
+After binary search finishes:
+
+           e   s
+Array: ... | ...
+
+e = Last element on the LEFT side
+s = First element on the RIGHT side
+
+Ceiling = arr[s]
+Floor   = arr[e]
+*/
+>>>>>>> 929dd03 (Add floor and ceiling binary search solutions)
