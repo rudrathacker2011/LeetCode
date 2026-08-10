9fcd44b7c477452e340c3dc33ffce5137c2dc4c3
Rudra Thacker <thacker.rudra20@gmail.com>
2026-08-10 21:17:55 +0530

    Sync LeetCode solutions

---
+++ peak-index-in-a-mountain-array/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int s = 0;
+        int e = arr.length-1;
+        while(s<e){
+            int m = s+(e-s)/2;
+            if(arr[m]>arr[m+1])
+            e = m;
+            else if(arr[m]<arr[m+1])
+            s = m+1;
+        }
+        return s;
+    }
+}
